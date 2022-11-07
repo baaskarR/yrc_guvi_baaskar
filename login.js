597c405a02094f09382103ea0e828e93b153673a
baaskarR <117639061+baaskarR@users.noreply.github.com>
2022-11-07 19:40:54 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,36 @@
+
+$("#btn-login").on("click", function(){
+
+    var _email = $("#email").val().trim();
+    var _pass =  $("#password").val().trim();
+
+
+    if(_pass == "" || _email == "" ){
+        alert("All fields are mandatory")
+    }
+
+    var signupJson = {
+        'email' : _email,
+        'password' : _pass,
+    }
+    $.ajax(
+        {
+            type: "POST",
+            url: "/api/login.php",
+            data: JSON.stringify(signupJson),
+            success: function(response){
+                var responseData = JSON.parse(JSON.stringify(response));
+                localStorage.setItem('session', responseData['session']);
+                window.location = "/profile.html";
+            },
+            error: function(xhr, status, error){
+                console.log(xhr);
+                console.log(status);
+                console.log(error);
+                alert("error");
+                var response = JSON.parse(xhr.responseText);
+                alert(response['error']);
+            }
+        }
+    );
+});
